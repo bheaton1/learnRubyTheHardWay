26bc9abc06d8bd81ea1369141459370f83e4de43
brittany heaton <bheaton@arrayschool.com>
2017-01-03 14:53:06 -0700

    completed ex 15 study drills

--- ex15.rb
+++ ex15.rb
@@ -1,13 +1,14 @@
-filename = ARGV.first
+filename = ARGV.first #using ARGV to get a filename
 
-txt = open(filename)
+txt = open(filename) #opening the file
 
-puts "Here's your file #{filename}:"
-print txt.read
+puts "Here's your file #{filename}:" #printing message and file name
+print txt.read #printing the text of the file
 
-print "Type the filename again: "
-file_again = $stdin.gets.chomp
+print "Type the filename again: " #another printed message
+file_again = $stdin.gets.chomp #using the same data(filename) to recale the file
 
-txt_again = open(file_again)
+txt_again = open(file_again) #reopening the file
 
-print txt_again.read
+
+print txt_again.read #reprinting the file text
