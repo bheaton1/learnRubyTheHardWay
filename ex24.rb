b6e66ea8bd65c161fe11a1c1ea1f14f04e04a58a
brittany heaton <bheaton@arrayschool.com>
2017-01-04 15:27:00 -0700

    finished ex26 test.

--- ex24.rb
+++ ex24.rb
@@ -19,6 +19,7 @@ puts "--------------"
 five = 10 - 2 + 3 - 6
 puts "This should be five: #{five}"
 
+
 def secret_formula(started)
   jelly_beans = started * 500
   jars = jelly_beans / 1000
--- ex26.rb
+++ ex26.rb
@@ -1,9 +1,9 @@
-module Ex2
+module Ex25
 
   # This function will break up words for us. (II)
   def Ex25.break_words(stuff)
     words = stuff.split(' ')
-    return word
+    return words
   end
 
   # Sorts the words.(I)
@@ -39,10 +39,11 @@ module Ex2
   # Sorts the words then prints the first and last one.(0)
   def Ex25.print_first_and_last_sorted(sentence)
     words = Ex25.sort_sentence(sentence)
-    Ex25.print_fist_word(words)
+    Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
-
+  
+end
 
 
 puts "Let's practice everything."
@@ -63,13 +64,13 @@ puts poem
 puts "--------------"
 
 
-five = 10 - 2 + 3 - 6 #(I)
-puts "This should be five: #{five}" #(I)
+five = 10 - 2 + 3 - 6
+puts "This should be five: #{five}"
 
 def secret_formula(started)
-  jelly_bens = started * 500
+  jelly_beans = started * 500
   jars = jelly_beans / 1000
-  crate = jars / 100
+  crates = jars / 100
   return jelly_beans, jars, crates
 end
 
